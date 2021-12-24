78e4eeea6d970b22b801b2680ecf9a2a1da62414
Md. Akib Hossain Omi <akib200045@diit.edu.bd>
2021-12-24 12:36:16 +0600

    Delete URI Online Judge/Beginner directory

--- URI Online Judge/Beginner/1011.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
- 
-int main() 
-{
-    double a;
-    scanf("%lf",&a);
-    printf("VOLUME = %.3lf\n",(4.0/3)*3.14159*a*a*a);
-    return 0;
-}
--- URI Online Judge/Beginner/1015.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main() 
-{
- double a,b,c,d;
- scanf("%lf%lf%lf%lf",&a,&b,&c,&d);
- printf("%.4lf\n",pow((a-c)*(a-c)+(b-d)*(b-d),0.5));
- return 0;
-}
