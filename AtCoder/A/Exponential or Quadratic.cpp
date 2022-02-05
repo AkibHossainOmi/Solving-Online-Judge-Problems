0cc766cfa405f01eb817965c3173e8b954053f96
Md. Akib Hossain Omi <omio1459@gmail.com>
2022-02-05 19:43:11 +0600

    Add files via upload

---
+++ AtCoder/A/Exponential or Quadratic.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main()
+{
+    ll n;
+    scanf("%lld",&n);
+    if(n<2||n>4) printf("Yes\n");
+    else printf("No\n");
+    return 0;
+}
