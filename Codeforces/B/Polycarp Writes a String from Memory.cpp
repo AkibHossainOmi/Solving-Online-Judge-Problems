5f0f3556b1eb22f342d3fcfab4c7adb07b2fdb97
Md. Akib Hossain Omi <omio1459@gmail.com>
2022-08-19 13:05:34 +0600

    Create Polycarp Writes a String from Memory.cpp

---
+++ Codeforces/B/Polycarp Writes a String from Memory.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    char s[2*100000+5];
+    int t, a[200];
+    cin>>t;
+    while(t--)
+    {
+        for(int j=97;j<=122;j++)
+            a[j]=0;
+        cin>>s;
+        int l=strlen(s), c=0, d=0;
+        for(int i=0;i<l;i++)
+        {
+            if(a[s[i]]==0)
+            {
+                a[s[i]]++;
+
+                c++;
+            }
+            if(c==4)
+            {
+                for(int j=97;j<=122;j++)
+                    a[j]=0;
+                a[s[i]]=1;
+                c=1;
+                d++;
+            }
+
+        }
+        if(c>0) d++;
+        cout<<d<<endl;
+    }
+    return 0;
+}
