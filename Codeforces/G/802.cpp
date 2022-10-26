3f7d2662f6d282dddd6808c24558539cdaa7bb93
Md. Akib Hossain Omi <omio1459@gmail.com>
2022-10-27 04:59:12 +0600

    Create 802.cpp

---
+++ Codeforces/G/802.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    char s[1005], g[6]="heidi";
+    cin>>s;
+    int j=0;
+    for(int i=0;i<strlen(s);i++)
+    {
+        if(s[i]==g[j]) j++;
+    }
+    if(j==5) cout<<"YES\n";
+    else cout<<"NO\n";
+}
