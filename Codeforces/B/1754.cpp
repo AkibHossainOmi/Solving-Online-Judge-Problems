b6e52c5594c966bf2680e8d8b84491db7f56bc3e
AkibHossainOmi <omio1459@gmail.com>
2022-11-05 09:48:02 +0600

    Renamed Best Permutaion to 1728 and 1746, 1754 is added

---
+++ Codeforces/B/1754.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, x, r=0;
+        cin>>n;
+        int a[n];
+        x=n/2;
+        for(int j=x; j>=1; j--)
+        {
+            for(int i=j; i<=n; i+=x)
+            {
+                a[r]=i, r++;
+                cout<<a[r-1]<<" ";
+            }
+        }
+
+
+        cout<<endl;
+    }
+}
